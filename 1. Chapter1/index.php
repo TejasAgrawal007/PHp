5e754babc2b631dd09ea187e8a3eaf2e2889a797
Tejas Agrawal <tejasagrawal36@gmail.com>
2022-01-18 00:41:15 +0530

    first commit

---
+++ 1. Chapter1/index.php
@@ -0,0 +1,42 @@
+<?php
+
+
+// 1. Print
+echo "Hello There i am Learning PHp";
+
+echo "<br>";
+
+// 2.  Vairbales
+$name = "Tejas Agrawal";
+echo "I Love $name";
+
+echo "<br>";
+echo "<br>";
+
+$number = 07;
+var_dump($number);
+
+echo "<br>";
+echo "<br>";
+
+$flotingPoint = 56.3;
+var_dump($flotingPoint);
+
+echo "<br>";
+echo "<br>";
+
+$x = True;
+var_dump($x);
+
+echo "<br>";
+echo "<br>";
+
+$cars = array("BMW", "Marcidi", "GTA", "Lambo");
+var_dump($cars);
+
+
+
+
+
+
+?>
