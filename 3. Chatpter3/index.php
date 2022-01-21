d821d5e93c0cd2532c4d1abfdfab6f694e6d85d4
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2022-01-21 17:32:59 +0530

    Add files via upload

---
+++ 3. Chatpter3/index.php
@@ -0,0 +1,58 @@
+<?php
+
+
+// One Thing You Notice About PHP is that its provide a data Type Conversion.
+
+/*
+    PHp is following a function to Type Conversion
+        A) is_int();
+        B) is_integer();
+        c) is_long();
+*/
+
+
+// $num1 = 34561;
+// var_dump(is_int($num1));
+// echo "<br>";
+
+// $num2 = 10.365;
+// var_dump(is_float($num2));
+// echo "<br>";
+
+
+
+
+
+// ***Math Object***
+
+// 1. PI()
+// echo (pi());
+
+
+
+// 2. MIN/MAX()
+// echo (min(1,25,250, 36,89));
+// echo "<br>";
+// echo (max(1,25,250, 36,89));
+
+
+//3. abs();
+// echo (abs(-6.7));
+
+
+// 4. sqrt();
+// echo (sqrt((64)));
+
+
+// 5. round()
+// echo (round(0.60));
+
+
+// 6. random();
+// echo (rand());
+
+
+
+
+
+?>
