88bb9b36d559ae4dd43d8ca3864a41c44a63f32c
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2022-01-21 16:57:53 +0530

    Add files via upload

---
+++ 2. Chapter2/index.php
@@ -0,0 +1,25 @@
+<?php
+
+// A String is a Sequence of Characters
+
+
+$str = "We are the Best Programmer!";
+// echo $str;
+
+// 1. strlen()
+// echo strlen($str);
+
+// 2. str_word_count
+// echo str_word_count($str);
+
+// 3. strrev()
+// echo strrev($str);
+
+// 4. strpos()
+// echo strpos("We are the Best php Programmer", "php");
+
+// 5. str_replace()
+// echo str_replace("Hello","Tejas", "Hey Tejas");
+
+
+?>
