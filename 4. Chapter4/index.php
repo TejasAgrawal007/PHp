6902ebc8b872f40cf72a8d13fe7fd5c0af592ad0
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2022-01-24 20:55:39 +0530

    Add files via upload

---
+++ 4. Chapter4/index.php
@@ -0,0 +1,43 @@
+<?php
+
+
+/* Operators are used to perform operations on variables and values.
+
+1) Arithmetic operators -
+                + - * / %
+
+2) Assignment operators - 
+                =  += -= *= /= %= 
+
+3) Comparison operators - 
+                == === != <> !== >= <= <=>
+
+
+4) Increment/Decrement operators - 
+                ++x, --x, --x,  x++ 
+
+
+5) Logical operators - 
+                &&(AND)  ||(OR)   !(NOT)
+
+
+6) String operators -   
+                .  .=
+
+
+
+7) Array operators -    
+                + == === != <> !==
+
+
+
+8) Conditional assignment operators - 
+                ?: ??
+
+*/
+
+
+
+
+
+?>
