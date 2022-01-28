d92a113dab68bfb7fac1f103fd1d6525705f0bc6
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2022-01-28 12:31:42 +0530

    Add files via upload

---
+++ 6. Chapter6/index.php
@@ -0,0 +1,65 @@
+<?php
+
+// echo "Welcome To The Function in Php";
+
+/*
+function -
+    A set of code with can be reuse and repeat a block of code again and agin is Known as afunction.
+
+    1) Build In
+    2) USer-define
+    3) Function Argument.
+
+
+
+     Syntex - 
+        function functionName(){
+            //logic...
+        }
+*/
+/*
+// Func declear
+function friend(){
+    echo "Hello Good Afternoon!";
+}
+
+// Func call
+friend();
+
+
+//  Func Argu declear
+function friend($fname){
+    echo "$fname  Good Afternoon!";
+}
+
+// Func call
+friend('Ayush');
+echo "<br>"; 
+
+
+
+friend('Siya');
+echo "<br>";
+
+
+
+friend('Akash');
+echo "<br>";
+
+
+
+friend('Vaishanavi');
+echo "<br>";
+*/
+
+
+// function getSum(int $a=2, int $b=3){
+//     // logic...
+//     return $a + $b;
+// }
+
+// echo getSum(1,2);
+
+
+
+?>
